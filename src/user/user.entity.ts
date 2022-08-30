c80193f26b52c78fed925b8441dadcf56f3fd5d1
Miguel Retroz <miguel.retroz@6place.com.br>
2022-08-30 11:11:24 -0300

    feat: add validation to the user class

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,8 +1,41 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class User {
   id: number;
+
+  // name
+  @IsNotEmpty({
+    message: 'name is required.',
+  })
+  @IsString({
+    message: 'name must be string.',
+  })
   name: string;
+
+  // email
+  @IsEmail({
+    message: 'invalid email',
+  })
   email: string;
+
+  // password
+  @IsNotEmpty({
+    message: 'password is required',
+  })
+  @IsString({
+    message: 'password must be string',
+  })
   password: string;
+
+  //fullName
+  @IsNotEmpty({
+    message: 'fullName is required',
+  })
+  @IsString({
+    message: 'fullName must be string',
+  })
   fullName: string;
+
+  // registerAt
   registerAt: Date;
 }
