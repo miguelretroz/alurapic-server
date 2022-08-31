d61344acdc12ac1dccbe1f0d747d23d5e921ee8a
Miguel Retroz <miguel.retroz@6place.com.br>
2022-08-31 10:20:50 -0300

    feat: implement exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { ClassSerializerInterceptor, Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { HttpExceptionFilter } from './common/filter/http-exception.filter';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule],
   controllers: [],
   providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
     {
       provide: APP_INTERCEPTOR,
       useClass: ClassSerializerInterceptor,
